199c98bd65cf8dca9abc3f9c3b3c5c0cdfeccf70
timeinfeldt <tim.einfeldt@gmail.com>
2017-08-31 20:11:48 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -24,71 +24,9 @@
       var h;
       return h = $(".splash"), h.cropit({
         imageBackground: !0,
+        smallImage: "allow",
         imageState: {
           src: "https://timeinfeldt.github.io/cropit/images/0-1920.jpg"
-        },
-        onZoomEnabled: d.bind(h),
-        onZoomDisabled: c.bind(h),
-        onImageError: b.bind(h.find(".cropit-preview"))
-      }), h.on("click", ".rotate-cw-btn", g.bind(h)), h.on("click", ".rotate-ccw-btn", f.bind(h)), h.on("click", ".select-image-btn", e.bind(h)), h.on("click", ".js-download-btn", a.bind(h))
-    }(),
-    function() {
-      var e;
-      return e = $(".demo-wrapper.basic"), e.cropit({
-        imageState: {
-          src: "https://timeinfeldt.github.io/cropit/images/1-960.jpg",
-          offset: {
-            x: 0,
-            y: -25
-          }
-        },
-        onZoomEnabled: d.bind(e),
-        onZoomDisabled: c.bind(e),
-        onImageError: b.bind(e.find(".cropit-preview"))
-      }), e.on("click", ".js-download-btn", a.bind(e))
-    }(),
-    function() {
-      var h;
-      return h = $(".demo-wrapper.custom-button"), h.cropit({
-        imageState: {
-          src: "https://timeinfeldt.github.io/cropit/images/2-960.jpg",
-          offset: {
-            x: 0,
-            y: -94
-          }
-        },
-        onZoomEnabled: d.bind(h),
-        onZoomDisabled: c.bind(h),
-        onImageError: b.bind(h.find(".cropit-preview"))
-      }), h.on("click", ".rotate-cw-btn", g.bind(h)), h.on("click", ".rotate-ccw-btn", f.bind(h)), h.on("click", ".select-image-btn", e.bind(h)), h.on("click", ".js-download-btn", a.bind(h))
-    }(),
-    function() {
-      var h;
-      return h = $(".demo-wrapper.image-background"), h.cropit({
-        imageBackground: !0,
-        imageState: {
-          src: "https://timeinfeldt.github.io/cropit/images/3-960.jpg",
-          offset: {
-            x: 0,
-            y: -86
-          }
-        },
-        onZoomEnabled: d.bind(h),
-        onZoomDisabled: c.bind(h),
-        onImageError: b.bind(h.find(".cropit-preview"))
-      }), h.on("click", ".rotate-cw-btn", g.bind(h)), h.on("click", ".rotate-ccw-btn", f.bind(h)), h.on("click", ".select-image-btn", e.bind(h)), h.on("click", ".js-download-btn", a.bind(h))
-    }(),
-    function() {
-      var h;
-      return h = $(".demo-wrapper.image-background-border"), h.cropit({
-        imageBackground: !0,
-        imageBackgroundBorderWidth: 15,
-        imageState: {
-          src: "https://timeinfeldt.github.io/cropit/images/4-960.jpg",
-          offset: {
-            x: 0,
-            y: -154
-          }
         },
         onZoomEnabled: d.bind(h),
         onZoomDisabled: c.bind(h),
